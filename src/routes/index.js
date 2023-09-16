8957972a220c617cf226ddeddf90947ad917d784
SychAndrii <asych@myseneca.ca>
2023-09-16 19:31:04 -0400

    Added testing api

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+const {Router} = require('express');
+const { author, version } = require('../../package.json');
+
+const router = Router();
+
+router.use('/v1', require('./api'));
+
+
+// Define a simple health check route. If the server is running
+// we'll respond with a 200 OK.  If not, the server isn't healthy.
+router.get('/', (req, res) => {
+  // Clients shouldn't cache this response (always request it fresh)
+  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
+  res.setHeader('Cache-Control', 'no-cache');
+
+  // Send a 200 'OK' response with info about our repo
+  res.status(200).json({
+    status: 'ok',
+    author,
+    // TODO: change this to use your GitHub username!
+    githubUrl: 'https://github.com/SychAndrii/fragments.git',
+    version,
+  });
+});
+
+module.exports = router;
