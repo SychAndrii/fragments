5d2477bdf2b96f49d8966132ea6568ec6e85bd4e
SychAndrii <asych@myseneca.ca>
2023-09-22 02:37:23 -0400

    Added testing using basic authentication strategy

---
+++ tests/unit/app.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('GET non existing route', () => {
+
+    test('non existing route returns 404 status code', () => request(app).get('/whatever').expect(404));
+  
+    test('non existing route returns object with error status', async () => {
+        const res = await request(app).get('/whatever');
+        expect(res.body.status).toBe('error');
+    });
+
+    test('non existing route returns object with error message and code', async () => {
+        const res = await request(app).get('/whatever');
+        expect(res.body.error.message).toBe('not found');
+        expect(res.body.error.code).toBe(404);
+    });
+    
+});
