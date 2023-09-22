314984fe413bda170bb6cbb59dfbee617b9def37
SychAndrii <asych@myseneca.ca>
2023-09-22 02:49:44 -0400

    Refactored routes to make use of responses defined in src/response.js

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const pino = require('pino-http')({
 });
 const passport = require('passport');
 const authenticate = require('./auth');
+const { createErrorResponse } = require('./response');
 
 // Create an express app instance we can use to attach middleware and HTTP routes
 const app = express();
@@ -57,13 +58,7 @@ app.use((err, req, res, next) => {
     logger.error({ err }, `Error processing request`);
   }
 
-  res.status(status).json({
-    status: 'error',
-    error: {
-      message,
-      code: status,
-    },
-  });
+  res.status(status).json(createErrorResponse(status, message));
 });
 
 // Export our `app` so we can access it in server.js
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const { author, version } = require('../../package.json');
-const {authenticate} = require('../auth');
+const { authenticate } = require('../auth');
+const { createSuccessResponse } = require('../response');
 
 const router = Router();
 
 router.use('/v1', authenticate(), require('./api'));
-
 
 // Define a simple health check route. If the server is running
 // we'll respond with a 200 OK.  If not, the server isn't healthy.
@@ -15,13 +15,14 @@ router.get('/', (req, res) => {
   res.setHeader('Cache-Control', 'no-cache');
 
   // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // TODO: change this to use your GitHub username!
-    githubUrl: 'https://github.com/SychAndrii/fragments.git',
-    version,
-  });
+
+  res.status(200).json(
+    createSuccessResponse({
+      author,
+      githubUrl: 'https://github.com/SychAndrii/fragments.git',
+      version,
+    })
+  );
 });
 
 module.exports = router;
