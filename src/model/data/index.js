07adf9b3d6d3933ce8931ca588374705113c5cf8
SychAndrii <asych@myseneca.ca>
2023-11-21 03:13:11 -0500

    Added aws models

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,3 +1,4 @@
 // If the environment sets an AWS Region, we'll use AWS backend
 // services (S3, DynamoDB); otherwise, we'll use an in-memory db.
+console.log(process.env.AWS_REGION ? require('./aws') : require('./memory'));
 module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
